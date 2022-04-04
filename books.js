388dc381d01a664c41bb26688dcbfc48fea7dffd
MirMue <miriam.mueller558@gmail.com>
2022-04-04 12:00:42 +0200

    Neue Version, ohne jQuery, async false

---
+++ books.js
@@ -0,0 +1,32 @@
+var books = [
+    {
+        title: "Harry Potter und der Gefangene von Askaban",
+        ISBN_10: "3551354030",
+        ISBN_13: "9783551354037",
+    },
+    {
+        title: "Im Grunde gut",
+        ISBN_10: "349900416X",
+        ISBN_13: "9783499004162",
+    },
+    {
+        title: "Der dunkle Turm - Schwarz",
+        ISBN_10: "3453875567",
+        ISBN_13: "9783453875562",
+    },
+    {
+        title: "Die schwarzen Wasser der Seine",
+        ISBN_10: "3841201121",
+        ISBN_13: "9783841201126",
+    },
+    {
+        title: "The Magic of Lines II",
+        ISBN_10: "1908175737",
+        ISBN_13: "9781908175731",
+    },
+    {
+        title: "Die 13 1/2 Leben des Käpt'n Blaubär",
+        ISBN_10: "3641255228",
+        ISBN_13: "9783641255220",
+    },
+  ];
--- libriam.js
+++ libriam.js
@@ -1,50 +1,33 @@
-const container = document.querySelector('.container');
+document.getElementById('button').addEventListener('click', loadLib);
 
-var books = [
-  {
-      title: "Harry Potter und der Gefangene von Askaban",
-      ISBN_10: "3551354030",
-      ISBN_13: "9783551354037",
-  },
-  {
-      title: "Im Grunde gut",
-      ISBN_10: "349900416X",
-      ISBN_13: "9783499004162",
-  },
-  {
-    title: "Der dunkle Turm - Schwarz",
-    ISBN_10: "3453875567",
-    ISBN_13: "9783453875562",
-  },
-  {
-    title: "Die schwarzen Wasser der Seine",
-    ISBN_10: "3841201121",
-    ISBN_13: "9783841201126",
-  },
-  {
-    title: "The Magic of Lines II",
-    ISBN_10: "1908175737",
-    ISBN_13: "9781908175731",
-  },
-  {
-    title: "Die 13 1/2 Leben des Käpt'n Blaubär",
-    ISBN_10: "3641255228",
-    ISBN_13: "9783641255220",
-  },
-]
+function loadLib () {
+  for (let i=0; i<books.length; i++) {
+    var xhr = new XMLHttpRequest();
+    // open(type, file/URL, async true/false), letzteres für richtige Reihenfolge der Bücher wichtig
+    xhr.open('GET', `https://www.googleapis.com/books/v1/volumes?q=isbn:${books[i].ISBN_13}`, false);
 
-$(document).ready(function(){
-    for (let i=0; i<books.length; i++){
-      $.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${books[i].ISBN_13}`, function(data) {
-        const x = document.createElement('div');
-        if (data.items[0].volumeInfo.imageLinks) {
-          x.innerHTML = `<p>${data.items[0].volumeInfo.title}</p><img src="${data.items[0].volumeInfo.imageLinks.thumbnail}"></img>`;
+    // alternativ könnte hier onload verwendet werden, onreadystatechange ist vielleicht overkill
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        const newVolume = document.createElement('div');
+        const vol = JSON.parse(this.responseText);
+        if (vol.items[0].volumeInfo.imageLinks) {
+          newVolume.innerHTML = `<p>${vol.items[0].volumeInfo.title}</p>
+          <img src="${vol.items[0].volumeInfo.imageLinks.thumbnail}"></img>`;
         }
-        else if (data.items[0].volumeInfo.title) {
-          x.innerHTML = `<p>${data.items[0].volumeInfo.title}</p><img src="thumbnail_placeholder.jpg"></img>`;
+        else if (vol.items[0].volumeInfo.title) {
+          newVolume.innerHTML = `<p>${vol.items[0].volumeInfo.title}</p><img src="thumbnail_placeholder.jpg"></img>`
         }
-        else {console.log("Problem im if-statement")}
-        container.appendChild(x);
-      });
+        else {console.log('Error: volume hat weder Titel noch Coverbild')}
+        
+        document.getElementById('bookshelf').appendChild(newVolume);
+      }
     }
-    });
+
+    xhr.onerror = function () {
+      console.log('Request Error');
+    }
+    // Sendet request, ist notwendig
+    xhr.send();
+  }
+}
