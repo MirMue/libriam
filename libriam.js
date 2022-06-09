fd88375817ee38484d1d4f95a6b18b8f255aad71
MirMue <miriam.mueller558@gmail.com>
2022-06-09 17:20:50 +0200

    refactoring, bookData problem unsolved

--- libriam.js
+++ libriam.js
@@ -1,101 +1,99 @@
-/*
-To do:
-    - Dafür sorgen, dass die Elemente sich nicht bewegt, wenn der Popuptext geöffnet oder geschlossen wird
-    - Funktion erstellen, mit der man ein neues Buch zu books.js hinzufügen kann
-    - style.css bearbeiten, sodass es bei vielen Büchern einen Zeilenumbruch gibt
-*/
+fillBookshelf();
+initButtons();
 
-loadLib();
-checkButtons1();
-checkButtons2();
+// Startet den for-loop nicht. Das array bbookData ist aber vorhanden, also keine Ahnung warum.
+function fillBookshelf() {
+  const bookData = getBooks();
+  console.log(bookData)
+  for (const book of bookData) {
+    console.log('for-of-loop funktioniert')
+    const newVolume = document.createElement('div');
+    const thumbnail =  searchForThumbnail(book);
+    console.log(book.items[0].volumeInfo.title)
+    newVolume.innerHTML = createBookHtml(
+      book.items[0].volumeInfo.title,
+      book.items[0].volumeInfo.subtitle,
+      book.items[0].volumeInfo.authors,
+      book.items[0].volumeInfo.publishedDate,
+      thumbnail
+    )
+    document.getElementById('bookshelf').appendChild(newVolume);
+  }
+  console.log('fillBookshelf beendet')
+}
 
-function loadLib () {
+function searchForThumbnail(book) {
+  if (book.items[0].volumeInfo.imageLinks) {
+    return book.items[0].volumeInfo.imageLinks.thumbnail
+  } else {return 'empty'}
+}
+
+
+function getBooks () {
+  const bookData = [];
   for (let i=0; i<books.length; i++) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://www.googleapis.com/books/v1/volumes?q=isbn:${books[i].ISBN_13}`, false);
+    xhr.open('GET', `https://www.googleapis.com/books/v1/volumes?q=isbn:${books[i].ISBN_13}`, true);
 
-    xhr.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        const newVolume = document.createElement('div');
-        const vol = JSON.parse(this.responseText);
-        if (vol.items[0].volumeInfo.imageLinks) {
-          newVolume.innerHTML = `<img src="${vol.items[0].volumeInfo.imageLinks.thumbnail}"></img>
-          <button class="btn-show" id="btn1-${books[i].ISBN_13}">Mehr Infos...</button>
-          <button class="btn-hide" id="btn2-${books[i].ISBN_13}">Weniger Infos...</button>
-          <p class="popup-hide" id="pop-${books[i].ISBN_13}">${vol.items[0].volumeInfo.authors}<br>-<br>
-          ${vol.items[0].volumeInfo.title}. ${vol.items[0].volumeInfo.subtitle}<br>-<br>${vol.items[0].volumeInfo.publishedDate}</p>`;
-        }
-        else if (vol.items[0].volumeInfo.title) {
-          newVolume.innerHTML = `<p>${vol.items[0].volumeInfo.title}</p><img src="thumbnail_placeholder.jpg"></img>
-          <button class="btn-show" id="btn1-${books[i].ISBN_13}">Mehr Infos...</button>
-          <button class="btn-hide" id="btn2-${books[i].ISBN_13}">Weniger Infos...</button>
-          <p class="popup-hide" id="pop-${books[i].ISBN_13}">${vol.items[0].volumeInfo.authors}<br>-<br>
-          ${vol.items[0].volumeInfo.title}. ${vol.items[0].volumeInfo.subtitle}<br>-<br>${vol.items[0].volumeInfo.publishedDate}</p>`
-        }
-        else {console.log('Error: volume hat weder Titel noch Coverbild')}
-        
-        document.getElementById('bookshelf').appendChild(newVolume);
-      }
+    xhr.onload = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          bookData.push(JSON.parse(this.responseText))
+        } else {console.log('Http Request Error 1 - xhr.statusText: ', xhr.statusText)}
+      } else {console.log('Http Request Error 2 - xhr.statusText: ', xhr.statusText)}
     }
 
+    // xhr.onreadystatechange = () => {
+    //   if (this.readyState == 4 && this.status == 200) {
+    //     console.log('xhr.onreadystatechange erreicht')
+    //     bookData.push(JSON.parse(this.responseText))
+    //   }
+    // }
+
     xhr.onerror = function () {
-      console.log('Request Error');
+      console.log('Http Request Error 3 - xhr.statusText: ', xhr.statusText);
     }
 
     xhr.send();
   }
+  return bookData
 }
 
-function checkButtons1 () {
-  const buttons = document.querySelectorAll('.btn-show').length;
+function createBookHtml (title, subtitle, authors, publishedDate, thumbnail) {
+  let html = '';
+  if (thumbnail !== 'empty') {
+    html = `<img src="${thumbnail}">`
+  } else if (thumbnail === 'empty') {
+    html = `<p>${title}</p><img src="thumbnail_placeholder.jpg">`
+  } else {console.log('Fehler: thumbnail weder vorhanden noch empty')}
 
-  for (let i=0; i<buttons; i++) {
-    document.querySelectorAll('.btn-show')[i].addEventListener('click', function () {
-      if (document.getElementById(`pop-${books[i].ISBN_13}`).className == 'popup-hide' &&
-      document.getElementById(`pop-${books[i].ISBN_13}`).className != 'popup-show') {
-        document.getElementById(`pop-${books[i].ISBN_13}`).className = 'popup-show';
-        document.getElementById(`btn1-${books[i].ISBN_13}`).className = 'btn-hide';
-        document.getElementById(`btn2-${books[i].ISBN_13}`).className = 'btn-show';
-      }
-      if (document.getElementById(`pop-${books[i].ISBN_13}`).className = 'popup-show' &&
-      document.getElementById(`pop-${books[i].ISBN_13}`).className != 'popup-hide') {
-        
-      }
-      else {console.log('Popup Error 1')}
+  html += `<button class="btn-info">Mehr Infos...</button>
+    <button class="btn-close">Weniger Infos...</button>
+    <p class="hidden">${authors}<br>-<br>
+    ${title}. ${subtitle}<br>-<br>${publishedDate}</p>`
+
+  return html
+}
+
+function initButtons () {
+  const infoButtons = document.querySelectorAll('.btn-info');
+  const closeButtons = document.querySelectorAll('.btn-close');
+
+  for (const button of infoButtons) {
+    button.addEventListener('click', () => {
+      const hiddenElement = button.parentElement.getElementsByClassName('hidden');
+      hiddenElement[0].className = 'visible';
+      button.style.backgroundColor = 'grey';
+      button.parentElement.querySelector('.btn-close').style.backgroundColor = 'goldenrod';
+    })
+  }
+
+  for (const button of closeButtons) {
+    button.addEventListener('click', () => {
+      const visibleElement = button.parentElement.getElementsByClassName('visible');
+      visibleElement[0].className = 'hidden';
+      button.style.backgroundColor = 'grey';
+      button.parentElement.querySelector('.btn-info').style.backgroundColor = 'goldenrod';
     })
   }
 }
-
-// Das if-statement hat mit der Bedingung, dass der Popuptext-classname == 'pop-show' ist, nicht funktioniert.
-// Wird der Popuptext-classname in checkButtons2 abgerufen, ist er true, nicht popup-show.
-// Innerhalb von checkButtons1 wird der classname aber korrekterweise auf popup-show gesetzt.
-// Habe die if-statement-Bedingung in checkButtons2 jetzt einfach auf classname == 'true' gesetzt.
-function checkButtons2 () {
-  const buttons = document.querySelectorAll('.btn-hide').length;
-
-  for (let i=0; i<buttons; i++) {
-    document.querySelectorAll('.btn-hide', )[i].addEventListener('click', function () {
-      if (document.getElementById(`pop-${books[i].ISBN_13}`).className == 'true') {
-        document.getElementById(`pop-${books[i].ISBN_13}`).className = 'popup-hide';
-        document.getElementById(`btn2-${books[i].ISBN_13}`).className = 'btn-hide';
-        document.getElementById(`btn1-${books[i].ISBN_13}`).className = 'btn-show';
-      }
-      else {console.log('Popup Error 2')}
-    })
-  }
-}
-
-// Funktion und Eventlistener zum Überprüfen der classnames:
-// (Zum Verwenden den button in index.html einkommentieren)
-// document.querySelector('#checkbtn').addEventListener('click', checkClassname);
-// function checkClassname() {
-//   const buttons = document.querySelectorAll('.btn-hide').length;
-
-//   for (let i=0; i<buttons; i++) {
-//     console.log('i: ', i)
-//     console.log('classname popuptext: ', document.querySelector(`#pop-${books[i].ISBN_13}`).className, typeof document.querySelector(`#pop-${books[i].ISBN_13}`).className);
-//     console.log('classname button "Mehr...": ', document.getElementById(`btn1-${books[i].ISBN_13}`).className);
-//     console.log('classname button "Weniger...": ', document.getElementById(`btn2-${books[i].ISBN_13}`).className);
-//   }
-//   console.log('-------------------------------------')
-// }
