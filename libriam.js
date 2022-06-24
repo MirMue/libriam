f7d83ce41534872fa2db73b828e235afe142c953
MirMue <miriam.mueller558@gmail.com>
2022-06-24 16:10:47 +0200

    Modals implemented, bookcovers as buttons, style changed

--- libriam.js
+++ libriam.js
@@ -1,5 +1,8 @@
-getBooks().then((response) => fillBookshelf(response));
-initButtons();
+getBooks()
+  .then((response) => fillBookshelf(response))
+  .then((response) => initButtons(response));
+
+  // Get data from Google Books and fill bookshelf with book elements:
 
 async function getBooks () {
   const bookData = [];
@@ -20,48 +23,76 @@ function fillBookshelf(bookData) {
       book.items[0].volumeInfo.subtitle,
       book.items[0].volumeInfo.authors,
       book.items[0].volumeInfo.publishedDate,
-      book.items[0].volumeInfo.imageLinks
+      book.items[0].volumeInfo.imageLinks,
+      book.items[0].id
     );
 
-    document.getElementById('bookshelf').appendChild(newVolume);
+    document.getElementById('bookshelf').appendChild(newVolume)
   }
 }
 
-function createBookHtml (title, subtitle, authors, publishedDate, imageLinks) {
+function createBookHtml (title, subtitle, authors, publishedDate, imageLinks, volumeId) {
   let html = '';
+  let bookCover = '';
+  const publishedYear = publishedDate.split('-')[0]
+
+  // thumbnail_placeholder.jpg soll noch durch canvas mit Buchtitel ersetzt werden
   if (imageLinks) {
-    html = `<img src="${imageLinks.thumbnail}">`
+    bookCover += `<img class="bookcover" src="${imageLinks.thumbnail}">`
   } else {
-    html = `<p>${title}</p><img src="thumbnail_placeholder.jpg">`
+    bookCover += `<img class="bookcover" src="thumbnail_placeholder.jpg">`
   }
 
-  html += `<button class="btn-info">Mehr Infos...</button>
-    <button class="btn-close">Weniger Infos...</button>
-    <p class="hidden">${authors}<br>-<br>
-    ${title}. ${subtitle}<br>-<br>${publishedDate}</p>`
+  if (subtitle == undefined) {
+    html += `<div class="book"><button class="book-button" data-modal-target="#modal-${volumeId}">${bookCover}</button><div class="modal" id="modal-${volumeId}">
+    <div class="modal-header"><div class="modal-title">Buchinfos</div><button data-close-button class="modal-button">&times;</button></div>
+    <div class="modal-body">${authors}<br>${title}<br>${publishedYear}</div></div></div>`
+  } else {
+    html += `<div class="book"><button class="book-button" data-modal-target="#modal-${volumeId}">${bookCover}</button><div class="modal" id="modal-${volumeId}">
+    <div class="modal-header"><div class="modal-title">Buchinfos</div><button data-close-button class="modal-button">&times;</button></div>
+    <div class="modal-body">${authors}<br>${title}<br>${subtitle}<br>${publishedYear}</div></div></div>`
+  }
 
   return html
 }
 
+// Open modals by clicking on bookcovers:
+
 function initButtons () {
-  const infoButtons = document.querySelectorAll('.btn-info');
-  const closeButtons = document.querySelectorAll('.btn-close');
+  const openModalButtons = document.querySelectorAll('[data-modal-target]');
+  const closeModalButtons = document.querySelectorAll('[data-close-button]');
+  const overlay = document.getElementById('overlay');
 
-  for (const button of infoButtons) {
+  openModalButtons.forEach(button => {
     button.addEventListener('click', () => {
-      const hiddenElement = button.parentElement.getElementsByClassName('hidden');
-      hiddenElement[0].className = 'visible';
-      button.style.backgroundColor = 'grey';
-      button.parentElement.querySelector('.btn-close').style.backgroundColor = 'goldenrod';
+      const modal = document.querySelector(button.dataset.modalTarget);
+      openModal(modal);
     })
-  }
+  })
 
-  for (const button of closeButtons) {
+  closeModalButtons.forEach(button => {
     button.addEventListener('click', () => {
-      const visibleElement = button.parentElement.getElementsByClassName('visible');
-      visibleElement[0].className = 'hidden';
-      button.style.backgroundColor = 'grey';
-      button.parentElement.querySelector('.btn-info').style.backgroundColor = 'goldenrod';
+      const modal = button.closest('.modal');
+      closeModal(modal);
     })
-  }
+  })
+
+  overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => {
+      closeModal(modal)
+    })
+  })
+}
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
 }
