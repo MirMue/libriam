2d72ca3e6c7e86097c96a334d781b86e2c70a5d8
MirMue <miriam.mueller558@gmail.com>
2022-10-27 19:04:02 +0200

    Created SQLite DB with table "books"

---
+++ backend/sql/libriamDB.sql
@@ -0,0 +1,27 @@
+-- CREATE TABLE books(
+-- id INTEGER PRIMARY KEY,
+-- googleLink TEXT,
+-- authors TEXT,
+-- title TEXT,
+-- subtitle TEXT,
+-- publishedYear TEXT,
+-- imgLink TEXT
+-- );
+
+-- Testweise manuell eingefügt:
+-- INSERT INTO books (
+-- googleLink,
+-- authors,
+-- title,
+-- publishedYear,
+-- imgLink
+-- ) 
+-- VALUES (
+-- 'https://www.googleapis.com/books/v1/volumes/Jmv6DwAAQBAJ',
+-- 'Stephen King',
+-- 'Rita Hayworth and Shawshank Redemption',
+-- '2020',
+-- 'http://books.google.com/books/publisher/content?id=Jmv6DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE70yVSOGWHqYi5agh2SwzMYtlbTLkBQOZ2D9E-6_rYqQ1tRlvBpdzuqBWjRYwA7uXDUuFgW58KHatSgpwyrjPTQc0PPG__qBedMbQ-Nba7gIzyhLDp93TsswC4HJvvdjFGTz3AAm&source=gbs_api'
+-- );
+
+SELECT * FROM books;
