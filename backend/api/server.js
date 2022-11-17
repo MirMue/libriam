ea927cb60b2a4418d9d9ced1dbb94aa0a1fd78a6
MirMue <miriam.mueller558@gmail.com>
2022-11-17 14:45:19 +0100

    Error handling in server.js

--- backend/api/server.js
+++ backend/api/server.js
@@ -23,52 +23,67 @@ app.use(
 
 // Gets all books
 app.get("/books", (req, res) => {
-  res.send(books);
+  try {
+    res.send(books);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ msg: "server error" });
+  }
 });
 
 // Saves book in books.json
 app.post("/books", (req, res) => {
-  // Checks wether new book is already saved in library
-  for (let i = 0; i < books.length; i++) {
-    if (books[i].id === req.body.id) {
-      return res.status(400).send({ msg: "doublet" });
+  try {
+    // Checks wether new book is already saved in library
+    for (let i = 0; i < books.length; i++) {
+      if (books[i].id === req.body.id) {
+        console.log("doublette");
+        console.log(req.body.id);
+        return res.status(400).send({ msg: "doublet" });
+      }
     }
+
+    // Adds book from search results to "books"-array
+    books.push(req.body);
+
+    // Stringifies book data for writeFile-method
+    const booksString = JSON.stringify(books, null, 2);
+
+    // Saves book data in book.json file
+    fs.writeFile("../backend/db/books.json", booksString, (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send({ msg: "error while saving to file" });
+      }
+      console.log("book saved");
+      return res.send();
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: "server error" });
   }
-
-  // Adds book from search results to "books"-array
-  books.push(req.body);
-
-  // Stringifies book data for writeFile-method
-  const booksString = JSON.stringify(books, null, 2);
-
-  // Saves book data in book.json file
-  fs.writeFile("../backend/db/books.json", booksString, (err) => {
-    if (err) {
-      console.log("Error: ", err);
-    } else {
-      console.log("Added new book to books.json");
-    }
-  });
-
-  res.send();
 });
 
 // Deletes book from books.json
 app.delete("/books/:id", (req, res) => {
-  // Removes book object from books array
-  const newBooksArray = books.filter((book) => book.id !== req.params.id);
+  try {
+    // Removes book object from books array
+    const newBooksArray = books.filter((book) => book.id !== req.params.id);
 
-  // Stringifies and formats the new books array
-  const newBooksString = JSON.stringify(newBooksArray, null, 2);
+    // Stringifies and formats the new books array
+    const newBooksString = JSON.stringify(newBooksArray, null, 2);
 
-  // Overwrites books.json with stringified new books array
-  fs.writeFile("../backend/db/books.json", newBooksString, (err) => {
-    if (err) {
-      console.log("error: ", err);
-    } else {
-      console.log("Deleted book from books.json");
-    }
-  });
-
-  res.send();
+    // Overwrites books.json with stringified new books array
+    fs.writeFile("../backend/db/books.json", newBooksString, (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send({ msg: "error while saving to file" });
+      }
+      console.log("book deleted");
+      return res.send();
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: "server error" });
+  }
 });
